e8e775d35e7595f5f5714f7371142ed2f1291557
brenroy999 <brenroy999@ed.amdsb.ca>
2018-11-01 14:49:32 -0400

    Add project files.

---
+++ MultiChoiceAdventure/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace MultiChoiceAdventure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.blueText = new System.Windows.Forms.Label();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.redText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(86, 17);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "OutputLabel";
+            // 
+            // blueText
+            // 
+            this.blueText.AutoSize = true;
+            this.blueText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueText.Location = new System.Drawing.Point(63, 363);
+            this.blueText.Name = "blueText";
+            this.blueText.Size = new System.Drawing.Size(83, 17);
+            this.blueText.TabIndex = 1;
+            this.blueText.Text = "Blue Output";
+            // 
+            // blueButton
+            // 
+            this.blueButton.Location = new System.Drawing.Point(17, 357);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(40, 30);
+            this.blueButton.TabIndex = 2;
+            this.blueButton.Text = "Blue";
+            this.blueButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Red";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // redText
+            // 
+            this.redText.AutoSize = true;
+            this.redText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redText.Location = new System.Drawing.Point(63, 327);
+            this.redText.Name = "redText";
+            this.redText.Size = new System.Drawing.Size(81, 17);
+            this.redText.TabIndex = 3;
+            this.redText.Text = "Red Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 399);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.redText);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.blueText);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label blueText;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label redText;
+    }
+}
+
---
+++ MultiChoiceAdventure/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiChoiceAdventure
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //
+            outputLabel.Text = "You begin daydreaming in class, and wake up to a sudden snap of a ruler on your desk. " +
+                "\nYou open your eyes, and everyone is missing, with no trace.";
+           blueText.Text="Investigate room.";
+            redText.Text="Leave Immediately.";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
+                }
+                else if (scene == 1 | scene == 4)
+                {
+                    scene = 5;
+                }
+                else if (scene == 2)
+                {
+                    scene = 4;
+                }
+                else if (scene == 5)
+                {
+                    scene = 6;
+                }
+                else if (scene == 6)
+                {
+                    scene = 8;
+                }
+                else if (scene == 8)
+                {
+                    scene = 10;
+                }
+                else if (scene == 10)
+                {
+                    scene = 11;
+                }
+                else if (scene == 11)
+                {
+                    scene = 12;
+                }
+                else if (scene == 12)
+                {
+                    scene = 15;
+                }
+                else if (scene == 15)
+                {
+                    scene = 16;
+                }
+                else if (scene == 16)
+                {
+                    scene = 18;
+                }
+                else if (scene == 17)
+                {
+                    scene = 20;
+                }
+                else if (scene == 18|scene == 19|scene == 20|scene == 21)
+                {
+                    scene = 22;
+                    outputLabel.Text = "Restart?";
+                    redText.Text = "Yes.";
+                    blueText.Text = "No.";
+                }
+            }
+        }
+    }
+}
