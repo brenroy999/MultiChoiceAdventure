a3dc55b07ba26dab1265eab8ba7d202aa0bb9dc3
brenroy999 <brenroy999@ed.amdsb.ca>
2018-11-01 14:57:12 -0400

    Added stuff

--- MultiChoiceAdventure/Form1.cs
+++ MultiChoiceAdventure/Form1.cs
@@ -26,7 +26,7 @@ namespace MultiChoiceAdventure
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.M)
+            if (e.KeyCode == Keys.M) //Red 
             {
                 if (scene == 0)
                 {
@@ -38,46 +38,79 @@ namespace MultiChoiceAdventure
                 else if (scene == 1 | scene == 4)
                 {
                     scene = 5;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 2)
                 {
                     scene = 4;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 5)
                 {
                     scene = 6;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 6)
                 {
                     scene = 8;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 8)
                 {
                     scene = 10;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 10)
                 {
                     scene = 11;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 11)
                 {
                     scene = 12;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 12)
                 {
                     scene = 15;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 15)
                 {
                     scene = 16;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 16)
                 {
                     scene = 18;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 17)
                 {
                     scene = 20;
+                    outputLabel.Text = "";
+                    redText.Text = "";
+                    blueText.Text = "";
                 }
                 else if (scene == 18|scene == 19|scene == 20|scene == 21)
                 {
